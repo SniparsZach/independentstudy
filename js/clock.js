e8c99556a4744279d8ad86fbbc696139a15e67ab
sniparszach <zhui.sh@mysummitps.org>
2016-09-09 05:30:02 +0000

    Added todays day to the clock

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,22 @@
 function startTime() {
-    var today = new Date();
-    var month = today.getMonth() + 1;
-    var day = today.getDate();
-    var year = today.getYear() + 1900;
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =
-    month + "/" + day + "/" + year + "  |  " + h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var today = new Date();
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+  var year = today.getYear() + 1900;
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =
+    days[today.getDay()] + " | " + month + "/" + day + "/" + year + "  |  " + h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {
+    i = "0" + i
+  }; // add zero in front of numbers < 10
+  return i;
 }
