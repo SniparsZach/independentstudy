2f00fa40756fd039c7de7785a9ab91486176db66
sniparszach <zhui.sh@mysummitps.org>
2016-11-02 16:15:48 +0000

    Changed picture for game background

---
+++ js/shootergame.js
@@ -0,0 +1,7 @@
+var myCanvas = document.getElementById("myCanvas");
+var player = [];
+var amde = [];
+var ctx = myCanvas.getContext("2d");
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
