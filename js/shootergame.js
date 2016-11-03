42f2e15b4572170f2bbf115695d247ad469f640e
sniparszach <zhui.sh@mysummitps.org>
2016-11-03 06:46:45 +0000

    Finished the first NVIDIA page

--- js/shootergame.js
+++ js/shootergame.js
@@ -1,17 +1,6 @@
-var mycanvas = document.getElementById("mycCnvas");
-var ctx = mycanvas.getContext("2d");
-var bullets = [];
-var enemies = [];
+var game = new Game();
 
-var player = {
-    xPos: 256,
-    yPos: 490,
-    height: 20,
-    width: 20,
-    goLeft: false,
-    goRight: false,
-    goUp: false,
-    goDown: false,
-    shootDefault: false,
-    canShoot: true,
+function init() {
+	if(game.init())
+		game.start();
 }
